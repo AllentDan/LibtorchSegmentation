2032593d31afd1ab09aefa745cdc75b485096b8b
Sergey V <41781097+0x0000dead@users.noreply.github.com>
2021-08-24 02:44:22 +0700

    avoid so many copies

--- src/Segmentor.h
+++ src/Segmentor.h
@@ -26,13 +26,13 @@ class Segmentor
 public:
 	Segmentor();
 	~Segmentor() {};
-	void Initialize(int gpu_id, int width, int height, std::vector<std::string> name_list,
+	void Initialize(int gpu_id, int width, int height, std::vector<std::string>& name_list,
 		std::string encoder_name, std::string pretrained_path);
 	void SetTrainTricks(trainTricks &tricks);
 	void Train(float learning_rate, int epochs, int batch_size,
 		std::string train_val_path, std::string image_type, std::string save_path);
 	void LoadWeight(std::string weight_path);
-	void Predict(cv::Mat image, std::string which_class);
+	void Predict(cv::Mat& image, const std::string& which_class);
 private:
 	int width = 512; int height = 512; std::vector<std::string> name_list;
 	torch::Device device = torch::Device(torch::kCPU);
@@ -48,7 +48,7 @@ Segmentor<Model>::Segmentor()
 };
 
 template <class Model>
-void Segmentor<Model>::Initialize(int gpu_id, int _width, int _height, std::vector<std::string> _name_list,
+void Segmentor<Model>::Initialize(int gpu_id, int _width, int _height, std::vector<std::string>& _name_list,
 	std::string encoder_name, std::string pretrained_path) {
 	width = _width;
 	height = _height;
@@ -211,7 +211,7 @@ void Segmentor<Model>::LoadWeight(std::string weight_path) {
 }
 
 template <class Model>
-void Segmentor<Model>::Predict(cv::Mat image, std::string which_class) {
+void Segmentor<Model>::Predict(cv::Mat& image, const std::string& which_class) {
 	cv::Mat srcImg = image.clone();
 	int which_class_index = -1;
 	for (int i = 0; i < name_list.size(); i++) {
